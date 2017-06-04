1eb66e53e2474630c497c5c097a0400cf2cdef1d
yuyang <yuyang@TinadeMacBook-Air.local>
2017-06-04 16:13:26 +0800

    waojobcotemplate

---
+++ wuyaojob_co_template/default_tool.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+##############################
+# for items.py
+
+class TxtItemExporter(CsvItemExporter):
+
+    def __init__(self, *args, **kwargs):
+        delimiter = settings.get('CSV_DELIMITER', ',')
+        kwargs['delimiter'] = delimiter
+
+        fields_to_export = settings.get('FIELDS_TO_EXPORT', [])
+        if fields_to_export:
+        	kwargs['fields_to_export'] = fields_to_export
+
+        super(TxtItemExporter, self).__init__(*args, **kwargs)
+
+##############################
+#for middlewares.py
+
+class RandomUserAgentMiddleware(object):
+    def __init__(self, crawler):
+        super(RandomUserAgentMiddleware, self).__init__()
+
+        self.ua = UserAgent()
+        self.per_proxy = crawler.settings.get('RANDOM_UA_PER_PROXY', False)
+        self.ua_type = crawler.settings.get('RANDOM_UA_TYPE', 'random')
+        self.proxy2ua = {}
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def process_request(self, request, spider):
+        def get_ua():
+            '''Gets random UA based on the type setting (random, firefox…)'''
+            return getattr(self.ua, self.ua_type)
+
+        if self.per_proxy:
+            proxy = request.meta.get('proxy')
+            if proxy not in self.proxy2ua:
+                self.proxy2ua[proxy] = get_ua()
+                logger.debug('Assign User-Agent %s to Proxy %s'
+                             % (self.proxy2ua[proxy], proxy))
+            request.headers.setdefault('User-Agent', self.proxy2ua[proxy])
+        else:
+            request.headers.setdefault('User-Agent', get_ua())
+
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+        request.meta["proxy"] = proxyServer
+
+        request.headers["Proxy-Authorization"] = proxyAuth
+
+#########################
+#for pipelines.py
+
+#########################
+# for settings.py
+
+#########################
+# for main spider python file
+
+
+
